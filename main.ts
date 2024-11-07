f173274fdb52e588516a0566d244272ea373412e
Luke Evanson <156948079+BlueGoat11@users.noreply.github.com>
2024-11-07 10:37:47 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,86 @@
+namespace SpriteKind {
+    export const startButton = SpriteKind.create()
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    startGame = 1
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Finish`, function (sprite, location) {
+    nextLevel += 1
+})
+function Shoot () {
+	
+}
+let Y_Velocity = 0
+let randLevel = 0
+let startGame = 0
+startGame = 0
+let startNotice = sprites.create(assets.image`Start Prompt`, SpriteKind.startButton)
+startNotice.setPosition(30, 57)
+startNotice.startEffect(effects.halo)
+scene.setBackgroundImage(assets.image`Title Screen`)
+while (startGame == 0) {
+    music.play(music.stringPlayable("C5 A B G A F G E ", 120), music.PlaybackMode.UntilDone)
+}
+scene.setBackgroundImage(assets.image`Game BG`)
+sprites.destroy(startNotice)
+game.splash("Welcome to JetMan ONE!")
+game.splash("Hold The A button to fly.")
+game.splash("This game goes on as long as you can avoid the obstacles.")
+let jetMan = sprites.create(assets.image`Idle`, SpriteKind.Player)
+jetMan.setStayInScreen(true)
+music.play(music.stringPlayable("C D C D C D C D ", 50), music.PlaybackMode.LoopingInBackground)
+info.setLife(5)
+let nextLevel = 1
+while (true) {
+    randLevel = randint(1, 5)
+    if (randLevel == 1) {
+        tiles.setCurrentTilemap(tilemap`Level1`)
+    }
+    if (randLevel == 2) {
+        tiles.setCurrentTilemap(tilemap`Level2`)
+    }
+    if (randLevel == 3) {
+        tiles.setCurrentTilemap(tilemap`Level1`)
+    }
+    if (randLevel == 4) {
+        tiles.setCurrentTilemap(tilemap`Level1`)
+    }
+    if (randLevel == 5) {
+        tiles.setCurrentTilemap(tilemap`Level1`)
+    }
+    nextLevel = 0
+    pauseUntil(() => nextLevel == 1)
+}
+forever(function () {
+    if (startGame == 1) {
+        if (controller.B.isPressed() == true) {
+            Shoot()
+            pause(100)
+        }
+    }
+})
+forever(function () {
+    if (startGame == 1) {
+        if (controller.A.isPressed() == true) {
+            jetMan.setImage(assets.image`Flying`)
+            Y_Velocity += -0.6
+        }
+    }
+})
+forever(function () {
+    Y_Velocity += 0.2
+    jetMan.y += Y_Velocity
+})
+forever(function () {
+    if (startGame == 1) {
+        if (controller.A.isPressed() == true) {
+            music.setVolume(40)
+            music.play(music.melodyPlayable(music.beamUp), music.PlaybackMode.UntilDone)
+        }
+    }
+})
+game.onUpdateInterval(100, function () {
+    if (startGame == 1) {
+        jetMan.setImage(assets.image`Idle 2`)
+    }
+})
